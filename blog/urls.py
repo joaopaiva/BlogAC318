bada665c4c37173dc70172b060da27955e06e609
João Paiva <4913487+joaopaiva@users.noreply.github.com>
2019-10-08 18:23:36 -0300

    Add delete function

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     url(r'^tinymce/', include('tinymce.urls')),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
